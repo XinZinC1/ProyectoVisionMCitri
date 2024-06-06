594795c1426f75003c83a64eae5d79c698658f2e
hector-ar-24 <hector.aguirre@usil.pe>
2024-06-05 23:59:01 -0500

    load

--- main_preproces1.py
+++ main_preproces1.py
@@ -9,89 +9,89 @@ from time import sleep
 
 # Función para aplicar random horizontal flipping
 def flip_image():
-    image_folder = r'HOJAS'
-    for filename in os.listdir(image_folder):
+    carpeta = r'HOJAS'
+    for filename in os.listdir(carpeta):
         if filename.endswith('.jpg') or filename.endswith('.png'):
-            image_path = os.path.join(image_folder, filename)
-            image = Image.open(image_path)
-            flipped_image = ImageOps.mirror(image)
-            flipped_image.save(os.path.join(image_folder, '1' + os.path.basename(image_path)))
+            ruta = os.path.join(carpeta, filename)
+            imagen = Image.open(ruta)
+            imagen_flip = ImageOps.mirror(imagen)
+            imagen_flip.save(os.path.join(carpeta, 'flip_' + os.path.basename(ruta)))
 def apply_hpf_filter():
-    folder_path = r"HOJAS"
+    carpeta_imagenes = r"HOJAS"
     # Itera sobre los archivos en la carpeta
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(carpeta_imagenes):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             # Construye la ruta completa del archivo
-            file_path = os.path.join(folder_path, filename)
+            ruta = os.path.join(carpeta_imagenes, filename)
             
             # Abre la imagen
-            image = cv2.imread(file_path)
+            imagen = cv2.imread(ruta)
             
             # Aplica el filtro paso alto
             kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-            image = cv2.filter2D(image, -1, kernel)
+            imagen = cv2.filter2D(imagen, -1, kernel)
             
             # Guarda la imagen editada con un nuevo nombre
-            new_filename = f"4{filename}"
-            new_file_path = os.path.join(folder_path, new_filename)
-            cv2.imwrite(new_file_path, image)
+            nombre_imagen = f"hpf_{filename}"
+            nuevo_ruta = os.path.join(carpeta_imagenes, nombre_imagen)
+            cv2.imwrite(nuevo_ruta, imagen)
 
 def adjust_image():
-    folder_path = r"HOJAS"
-    output_folder1=r"AJUSTE"
+    carpeta_imagenes = r"HOJAS"
+    carpeta_final=r"AJUSTE"
     # Parámetros de edición
-    brightness_factor = 1.25  # Ajusta el brillo (1.0 = sin cambios)
-    contrast_factor = 1.05  # Ajusta el contraste (1.0 = sin cambios)
-    highlights_factor = 1.05  # Ajusta las altas luces (1.0 = sin cambios)
-    shadows_factor = 1.05 # Ajusta las sombras (1.0 = sin cambios)
-    sharpness_factor = 0.15  # Ajusta la nitidez (1.0 = sin cambios)
+    brillo = 1.25  # Ajusta el brillo (1.0 = sin cambios)
+    contraste = 1.05  # Ajusta el contraste (1.0 = sin cambios)
+    altas_luces = 1.05  # Ajusta las altas luces (1.0 = sin cambios)
+    sombras = 1.05 # Ajusta las sombras (1.0 = sin cambios)
+    nitidez = 0.15  # Ajusta la nitidez (1.0 = sin cambios)
 
     # Itera sobre los archivos en la carpeta
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(carpeta_imagenes):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             # Construye la ruta completa del archivo
-            file_path = os.path.join(folder_path, filename)
+            file_path = os.path.join(carpeta_imagenes, filename)
             
             # Abre la imagen
             image = Image.open(file_path)
             
             # Ajusta el brillo
             brightness_enhancer = ImageEnhance.Brightness(image)
-            image = brightness_enhancer.enhance(brightness_factor)
+            image = brightness_enhancer.enhance(brillo)
             
             # Ajusta el contraste
             contrast_enhancer = ImageEnhance.Contrast(image)
-            image = contrast_enhancer.enhance(contrast_factor)
+            image = contrast_enhancer.enhance(contraste)
             
             # Ajusta las altas luces
             highlights_enhancer = ImageEnhance.Brightness(image)
-            image = highlights_enhancer.enhance(highlights_factor)
+            image = highlights_enhancer.enhance(altas_luces)
             
             # Ajusta las sombras
             shadows_enhancer = ImageEnhance.Brightness(image)
-            image = shadows_enhancer.enhance(shadows_factor)
+            image = shadows_enhancer.enhance(sombras)
             
             # Ajusta la nitidez
             sharpness_enhancer = ImageEnhance.Sharpness(image)
-            image = sharpness_enhancer.enhance(sharpness_factor)
+            image = sharpness_enhancer.enhance(nitidez)
             
             # Guarda la imagen editada con un nuevo nombre
-            new_filename = f"3{filename}"
-            new_file_path = os.path.join(output_folder1, new_filename)
+            new_filename = f"edit_{filename}"
+            new_file_path = os.path.join(carpeta_final, new_filename)
             image.save(new_file_path)
 
 
 
 # Ejecuta las funciones secuencialmente
-for i in tqdm(range(10)):
+for i in tqdm(range(1)):
     flip_image()
-    sleep(0.2)
-    print("Horizontal flip aplicado.")
-for i in tqdm(range(10)):
+    sleep(0.001)
+    print(" Horizontal flip aplicado.")
+for i in tqdm(range(1)):
     apply_hpf_filter()
-    sleep(0.2)
-    print("HPF aplicado.")
-for i in tqdm(range(10)):
+    sleep(0.001)
+    print(" HPF aplicado.")
+for i in tqdm(range(1)):
     adjust_image()
-    sleep(0.2)
-    print("Ajuste de imagenes culminado.")
+    sleep(0.001)
+    print(" Ajuste de imagenes culminado.")
